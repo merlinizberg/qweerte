2a21ca88ac87f0138480724978cd824d91a2553a
merlinizberg <merlinizberg@gmail.com>
2019-07-26 22:11:55 +0300

    Выполнил 4е задание

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,24 @@
 
 package com.company;
-
+import java.util.Scanner;
 public class Main {
-
     public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter some number: ");
+        int input = in.nextInt();
         System.out.println("Hello, world!");
+        System.out.println(bin2dec(String.valueOf(input)));
+        System.out.println("Проверка правильности " + Integer.toBinaryString(bin2dec(String.valueOf(input))) + "=" + bin2dec(String.valueOf(input)) );
+    }
+    public static int bin2dec(String binNumber) {
+        int result = 0;
+
+        for (int i = 0; i < binNumber.length(); i++) {
+
+            result += Math.pow(2, i) * (binNumber.charAt(i) == '1' ? 1 : 0);
+
+        }
+        return result;
     }
 }
