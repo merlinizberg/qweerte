3ba752695c40d2c3ea5a0126f3737f70671f8104
merlinizberg <merlinizberg@gmail.com>
2019-07-27 00:19:18 +0300

    Выполнил 5е задание

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,19 +7,15 @@ public class Main {
         Scanner in = new Scanner(System.in);
         System.out.println("Enter some number: ");
         int input = in.nextInt();
-        System.out.println("Hello, world!");
-        int i = bin2dec(String.valueOf(input));
-        System.out.println(i);
-        System.out.println("Проверка правильности " + Integer.toBinaryString(bin2dec(String.valueOf(input))) + "=" + bin2dec(String.valueOf(input)) );
-    }
-    public static int bin2dec(String binNumber) {
-        int result = 0;
-
-        for (int i = 0; i < binNumber.length(); i++) {
-
-            result += Math.pow(2, i) * (binNumber.charAt(i) == '1' ? 1 : 0);
-
-        }
-        return result;
+        System.out.println(input + " * 1 = " + input);
+        System.out.println(input + " * 1 = " + input * 2);
+        System.out.println(input + " * 3 = " + input * 3);
+        System.out.println(input + " * 4 = " + input * 4);
+        System.out.println(input + " * 5 = " + input * 5);
+        System.out.println(input + " * 6 = " + input * 6);
+        System.out.println(input + " * 7 = " + input * 7);
+        System.out.println(input + " * 8 = " + input * 8);
+        System.out.println(input + " * 9 = " + input * 9);
+        System.out.println(input + " * 10 = " + input * 10);
     }
 }
