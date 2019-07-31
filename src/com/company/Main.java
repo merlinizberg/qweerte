eae3139deaaf74b0c54259cdb59040b0d8d6817b
merlinizberg <merlinizberg@gmail.com>
2019-07-31 21:48:12 +0300

    Выполнил 9е задание

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,23 @@
 
 package com.company;
-//impor java.util.Scanner;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        for (int i = 1; i <= 50; i++) {
-            System.out.print(i + " ");
+
+        Scanner in = new Scanner(System.in);
+        int i;
+        System.out.print("Введите длину массива:   ");
+        i = in.nextInt();
+        int arr1[] = new int[i];
+        int arr2[] = new int[i];
+        for (int x = 0; x < i; x++) {
+            System.out.print("Введите значение массива в ячейке:  ");
+            arr1[x] = in.nextInt();
         }
+        for (int x = 0; x < i; x++) {
+            arr2[x] = arr1[x] * 2;
+            System.out.print(arr2[x] + " ");
+        }
+
     }
 }
