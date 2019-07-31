5a7b7237d1de653a0036245bbdeb0b021d74b926
merlinizberg <merlinizberg@gmail.com>
2019-07-31 21:23:02 +0300

    Выполнил 7е задание

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,26 +1,9 @@
 
 package com.company;
-import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-
-        Scanner in = new Scanner(System.in);
-        final int x = 4, y = 7, z = 18;
-        int number;
-        System.out.println("Введите значение: ");
-        number = in.nextInt();
-        switch (number) {
-            case x:
-                System.out.println("Данное значение имеется в константах");
-                break;
-            case y:
-                System.out.println("Данное значение имеется в константах");
-                break;
-            case z:
-                System.out.println("Данное значение имеется в константах");
-                break;
-            default:
-                System.out.println("Такой константы нет!");
+        for (int i = 1; i <= 50; i++) {
+            System.out.print(i + " ");
         }
     }
 }
