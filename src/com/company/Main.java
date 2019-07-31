7c19998c467163081678c179f702738074a80bfc
merlinizberg <merlinizberg@gmail.com>
2019-07-31 22:07:11 +0300

    Выполнил 10е задание

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,19 +5,24 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner in = new Scanner(System.in);
-        int i;
+        int i, j;
         System.out.print("Введите длину массива:   ");
         i = in.nextInt();
-        int arr1[] = new int[i];
-        int arr2[] = new int[i];
-        for (int x = 0; x < i; x++) {
-            System.out.print("Введите значение массива в ячейке:  ");
-            arr1[x] = in.nextInt();
+        System.out.print("Введите ширину массива:   ");
+        j = in.nextInt();
+        int arr1[][] = new int[i][j];
+        int arr2[][] = new int[i][j];
+        for (int y = 0; y < i; y++) {
+            for (int x = 0; x < j; x++) {
+                System.out.print("Введите значение массива в ячейке:  ");
+                arr1[x][y] = in.nextInt();
+            }
         }
-        for (int x = 0; x < i; x++) {
-            arr2[x] = arr1[x] * 2;
-            System.out.print(arr2[x] + " ");
+        for (int y = 0; y < i; y++) {
+            for (int x = 0; x < j; x++) {
+                arr2[x][y] = arr1[x][y] * 3;
+                System.out.print(arr2[x][y] + " ");
+            }
         }
-
     }
 }
