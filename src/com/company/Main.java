283ef5ef159e7f725c1e8829e79cb672e2d60a75
merlinizberg <merlinizberg@gmail.com>
2019-08-01 16:33:16 +0300

    Выполнил 11е задание

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,26 +3,8 @@ package com.company;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-
-        Scanner in = new Scanner(System.in);
-        int i, j;
-        System.out.print("Введите длину массива:   ");
-        i = in.nextInt();
-        System.out.print("Введите ширину массива:   ");
-        j = in.nextInt();
-        int arr1[][] = new int[i][j];
-        int arr2[][] = new int[i][j];
-        for (int y = 0; y < i; y++) {
-            for (int x = 0; x < j; x++) {
-                System.out.print("Введите значение массива в ячейке:  ");
-                arr1[x][y] = in.nextInt();
-            }
-        }
-        for (int y = 0; y < i; y++) {
-            for (int x = 0; x < j; x++) {
-                arr2[x][y] = arr1[x][y] * 3;
-                System.out.print(arr2[x][y] + " ");
-            }
-        }
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        System.out.println(s);
     }
 }
