0a5b67b340b25bcb953655a6b5df80d7345b7593
merlinizberg <merlinizberg@gmail.com>
2019-08-04 12:15:46 +0300

    Выполнил 12е задание

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        System.out.println("Введите строку");
         String s = scan.nextLine();
-        System.out.println(s);
+        System.out.println("Вывод строки без проблеов");
+        System.out.println(s.replaceAll("\\s+",""));
     }
 }
