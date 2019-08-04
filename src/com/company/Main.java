98a1fe7dfc555bf74817c044d9bfd1a2d0cc9574
merlinizberg <merlinizberg@gmail.com>
2019-08-04 13:50:16 +0300

    Выполнил 14е задание

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,16 +6,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Введите первую строку");
+        System.out.println("Введите число в строку");
         String s1 = scan.nextLine();
-        System.out.println("Введите вторую строку");
-        String s2 = scan.nextLine();
-        if(s1.length() > s2.length()){
-            System.out.println("Первая строка длиннее:" + s1);
-        }else
-            if(s1.length() < s2.length()){
-            System.out.println("Вторая строка длиннее:" + s2);
-        }else
-            System.out.println("Обе строки равны");
+        System.out.println("число с типом данных string: " + s1);
+        int x = Integer.parseInt(s1);
+        System.out.println("число с типом данных int: " + x);
+        double y = x;
+        System.out.println("число с типом данных double: " + y);
     }
 }
