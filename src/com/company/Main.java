71179fd80edee4a4db05348148ded5332aeaed3a
merlinizberg <merlinizberg@gmail.com>
2019-08-04 12:34:21 +0300

    Выполнил 13е задание

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,21 @@
 
 package com.company;
+import sun.security.util.Length;
+
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Введите строку");
-        String s = scan.nextLine();
-        System.out.println("Вывод строки без проблеов");
-        System.out.println(s.replaceAll("\\s+",""));
+        System.out.println("Введите первую строку");
+        String s1 = scan.nextLine();
+        System.out.println("Введите вторую строку");
+        String s2 = scan.nextLine();
+        if(s1.length() > s2.length()){
+            System.out.println("Первая строка длиннее:" + s1);
+        }else
+            if(s1.length() < s2.length()){
+            System.out.println("Вторая строка длиннее:" + s2);
+        }else
+            System.out.println("Обе строки равны");
     }
 }
