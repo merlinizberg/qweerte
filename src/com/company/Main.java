f3d55438d9fbc8bc0b8fd856d4ae612a39a03b05
merlinizberg <merlinizberg@gmail.com>
2019-08-07 19:43:51 +0300

    Выполнил 18е задание

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,8 +4,15 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
-     String s;
-        BufferedReader br =
+     String s, s1;
+        try(BufferedReader br = new BufferedReader(new FileReader("test.txt"))) {
+            while ((s = br.readLine()) != null) {
+                System.out.println(s);
+            }
+        } catch (IOException exc){
+            System.out.println("Ошибка ввода/вывода: " + exc);
+        }
+        BufferedReader br1 =
                 new BufferedReader(
                         new InputStreamReader(System.in));
         System.out.println("Признак конца строки 'stop' ");
@@ -13,13 +20,13 @@ public class Main {
     {
         do{
             System.out.print(": ");
-            s = br.readLine();
+            s1 = br1.readLine();
 
-            if(s.compareTo("stop") == 0) break;
+            if(s1.compareTo("stop") == 0) break;
 
-            s = s +"\r\n";
-            fw.write(s);
-        } while (s.compareTo("stop") != 0);
+            s1 = s1 +"\r\n";
+            fw.write(s1);
+        } while (s1.compareTo("stop") != 0);
     } catch (IOException exc){
         System.out.println("Ошибка ввода/вывода: " + exc);
     }
