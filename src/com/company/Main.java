e27b0b2fbf60cdf1407463c99635ff20091414a5
merlinizberg <merlinizberg@gmail.com>
2019-08-07 19:15:46 +0300

    Выполнил 16е задание

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,17 +1,16 @@
 
 package com.company;
-import sun.security.util.Length;
+import java.io.*;
 
-import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Введите число в строку");
-        String s1 = scan.nextLine();
-        System.out.println("число с типом данных string: " + s1);
-        int x = Integer.parseInt(s1);
-        System.out.println("число с типом данных int: " + x);
-        double y = x;
-        System.out.println("число с типом данных double: " + y);
+     String s;
+    try(BufferedReader br = new BufferedReader(new FileReader("test.txt"))) {
+        while ((s = br.readLine()) != null) {
+            System.out.println(s);
+        }
+    } catch (IOException exc){
+        System.out.println("Ошибка ввода/вывода: " + exc);
+    }
     }
 }
