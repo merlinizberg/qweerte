5ea9cdc3bbf48fbe67b6bfe3a2b599c4078ed89d
merlinizberg <merlinizberg@gmail.com>
2019-08-17 00:18:14 +0300

    Выполнил 1е итоговое задание

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,23 +1,19 @@
 package com.company;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.text.DecimalFormat;
-
-class Main {
+import java.util.Scanner;
+public class Main {
     public static void main(String[] args) {
 
-        try {
-            DecimalFormat df = new DecimalFormat("#.##");
-            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-            double rub, change, c;
-            System.out.print("Введите количество рублей:   ");
-            rub = Double.parseDouble(reader.readLine());
-            System.out.print("Введите курс конверсии:   ");
-            change = Double.parseDouble(reader.readLine());
-            System.out.print(df.format(rub / change));
-        } catch (IOException e) {
-            e.printStackTrace();
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите число в двочичном формате: ");
+        int input = in.nextInt();
+        int i = bin2dec(String.valueOf(input));
+        System.out.println(i);
+    }
+    public static int bin2dec(String binNumber) {
+        int result = 0;
+        for (int i = 0;  i < binNumber.length() ; i++) {
+            result += Math.pow(2, i) * (binNumber.charAt(i) == '1' ? 1 : 0);
         }
+        return result;
     }
 }
