f9a004dc4c5a3eed71593873baa7a40396391297
merlinizberg <merlinizberg@gmail.com>
2019-08-25 23:56:12 +0300

    Выполнил 3е итоговое задание

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,50 +1,45 @@
 package com.company;
-import java.util.ArrayList;
+import java.util.*;
+import java.io.*;
 
-class Main implements BoxInerface {
-
-    private ArrayList<Sweet> sweets = new ArrayList<>();
-    public static void main(String[] args){
-        Main box1 = new Main();
-        box1.addSweet(new Candy("Залупрошен", 14, 88, "Гей"));
-        box1.printBoxWeight();
-    }
-    @Override
-    public void addSweet(Sweet sweet) {
-        sweets.add(sweet);
-    }
-
-    @Override
-    public void removeSweet(int index) {
-        sweets.remove(index);
-    }
-
-    @Override
-    public void removeSweet() {
-        sweets.remove(sweets.size());
-    }
-
-    @Override
-    public void printBoxWeight() {
-        double weight = 0;
-        for(Sweet s : sweets){
-            weight += s.getWeight();
+public class Main {
+    public static void main(String[] args) {
+        try {
+            Scanner in = new Scanner(System.in);
+            System.out.println("Введите название файла: ");
+            String name = in.nextLine();
+            Scanner scanner = new Scanner(new File(name));
+            Map<String, Integer> statistics = new HashMap<>();
+            Set<String> words = new TreeSet<>();
+            while (scanner.hasNext()) {
+                String word = scanner.useDelimiter("\\s+").next();
+                words.add(word);
+                Integer count = statistics.get(word);
+                if (count == null) {
+                    count = 0;
+                }
+                statistics.put(word, ++count);
+            }
+            System.out.println("Отсортировано: " +words);
+            System.out.println("Статистика: " + statistics);
+            Iterator it = statistics.entrySet().iterator();
+            int max = 0;
+            while (it.hasNext()) {
+                Map.Entry pair = (Map.Entry)it.next();
+                if((int) pair.getValue() > max){
+                    max = (int) pair.getValue();
+                }
+            }
+            it = statistics.entrySet().iterator();
+            while (it.hasNext()) {
+                Map.Entry pair = (Map.Entry)it.next();
+                if((int) pair.getValue() == max){
+                    System.out.println("Слово с самой частой частотой: " + pair.getKey());
+                }
+            }
+        }catch(IOException ex){
+            System.out.println(ex.getMessage());
         }
-        System.out.println(weight);
-    }
 
-    @Override
-    public void printBoxPrice() {
-        double price = 0;
-        for(Sweet s : sweets){
-            price += s.getPrice();
-        }
-        System.out.println(price);
-    }
-
-    @Override
-    public void getAllBoxInformation() {
-        printBoxWeight();
-        printBoxPrice();
     }
 }
