a13ba20dc5d9470e73c60593db7c0681095573cd
merlinizberg <merlinizberg@gmail.com>
2019-08-28 12:08:31 +0300

    Доработал разделение по знакам препинания

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,7 +12,7 @@ public class Main {
             Map<String, Integer> statistics = new HashMap<>();
             Set<String> words = new TreeSet<>();
             while (scanner.hasNext()) {
-                String word = scanner.useDelimiter("\\s+|[^a-zа-я]+").next();
+                String word = scanner.useDelimiter("[\\W]+").next();
                 words.add(word);
                 Integer count = statistics.get(word);
                 if (count == null) {
@@ -20,24 +20,24 @@ public class Main {
                 }
                 statistics.put(word, ++count);
             }
-            System.out.println("Отсортировано: " +words);
+            System.out.println("Отсортировано: " + words);
             System.out.println("Статистика: " + statistics);
             Iterator it = statistics.entrySet().iterator();
             int max = 0;
             while (it.hasNext()) {
-                Map.Entry pair = (Map.Entry)it.next();
-                if((int) pair.getValue() > max){
+                Map.Entry pair = (Map.Entry) it.next();
+                if ((int) pair.getValue() > max) {
                     max = (int) pair.getValue();
                 }
             }
             it = statistics.entrySet().iterator();
             while (it.hasNext()) {
-                Map.Entry pair = (Map.Entry)it.next();
-                if((int) pair.getValue() == max){
-                    System.out.println("Слово с самой частой частотой: " + pair.getKey());
+                Map.Entry pair = (Map.Entry) it.next();
+                if ((int) pair.getValue() == max) {
+                    System.out.println("Слово с самой большой частотой: " + pair.getKey() + " С частотой= " + max);
                 }
             }
-        }catch(IOException ex){
+        } catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
 
